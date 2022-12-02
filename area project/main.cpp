e2c1e917da1ec778b8faf73711aec19f2b126199
ELNINOW <116989714+ELNINOW@users.noreply.github.com>
2022-12-02 13:58:03 +0530

    Add files via upload

---
+++ area project/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+
+int main(){
+    int room_width{0};
+    int room_height{0};
+    
+    
+    cout<<"Enter the room width"<<endl;
+    cin>> room_width;
+    cout<<"enter the room height"<<endl;
+    cin>> room_height;
+    cout<< room_height*room_width<<"square feet"<<endl;
+    
+    
+    
+    
+    
+    
+    return 0; 
+}
